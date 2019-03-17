843c6f86be8db9a7eda0c20479e146b07dbbd29c
Dimitar Marinov <d.marinov@blubito.com>
2019-03-17 23:21:23 +0200

    no message

--- src/app/services/pitch.service.ts
+++ src/app/services/pitch.service.ts
@@ -1,17 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { map, catchError } from 'rxjs/operators';
 import {environment} from '@env/environment';
-
-const routes = {
-    quote: (c: RandomQuoteContext) => `/jokes/random?category=${c.category}`
-};
-
-export interface RandomQuoteContext {
-    // The quote's category: 'dev', 'explicit'...
-    category: string;
-}
 
 @Injectable()
 export class PitchService {
@@ -19,11 +8,6 @@ export class PitchService {
     constructor(private httpClient: HttpClient) { }
 
     getPitchesList(pageNumber: number , pageSize: number) {
-
-        let headers = new HttpHeaders();
-        headers = headers.set('Content-Type', 'application/json');
-
-        console.log (environment.serverUrl + '/' + 'pitches') ;
 
         return this.httpClient.get(
             environment.serverUrl + '/' + 'pitches' + '?page[number]=' + pageNumber + '&page[size]=' + pageSize )
